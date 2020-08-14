da56ff89dcf20251c58840a4d9d5f01af0031732
fgduarte01 <fgduarte01@gmail.com>
2020-08-14 08:11:49 -0300

    commit de paginas navegacao

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 import Logo from '../../assets/img/nandaflix.png';
 import './Menu.css';
 // import ButtonLink from '../components/ButtonLink';
@@ -8,11 +9,11 @@ function Menu() {
 
   return (
       <nav className="Menu">
-          <a href="/">
+          <Link to="/">
             <img src={Logo} className="Logo" alt="nandaflix"/>
-          </a> 
+          </Link> 
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
            Novo Vídeo 
       </Button>
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import home from './pages/home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
+import CadastroVideo from './pages/cadastro/video'; 
+import CadastroCategoria from './pages/cadastro/categoria';
 
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <switch>
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route path="/" component={home} exact />
+    </switch>
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 
