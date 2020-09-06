74b7228f958be1ca86d294209f718fc63d94460f
fgduarte01 <fgduarte01@gmail.com>
2020-09-06 18:18:21 -0300

    commit

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -18,7 +18,9 @@ function CadastroCategoria() {
   const [Categorias, setCategorias] = useState([]);
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://nandaflix2.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
